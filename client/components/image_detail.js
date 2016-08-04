8127872a54d215dd1f4ef26128bb13ab1dbddaf8
rayGreycloud <rayGreycloud@gmail.com>
2016-08-04 16:35:13 -0700

    Add React plus image_list and image_detail components

---
+++ client/components/image_detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageDetail = (props) => {
+  return (
+    <li>
+    <img src={props.image.link} />
+    {props.image.title}
+    </li>
+  );
+};
+
+export default ImageDetail;
---
+++ client/components/image_list.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'http://dummyimage.com/600x400' }
+];
+
+
+const ImageList = () => {
+  const RenderedImages = IMAGES.map(function (image) {
+    return <ImageDetail image={image}/>
+  });
+
+  return (
+    <ul>
+      {RenderedImages}
+    </ul>
+  );
+};
+
+export default ImageList;
