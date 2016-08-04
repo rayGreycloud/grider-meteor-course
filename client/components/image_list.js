22a781b619c9417efbbf70c86c29c3bbfbb74b2e
rayGreycloud <rayGreycloud@gmail.com>
2016-08-04 16:51:03 -0700

    Add Bootstrap and styling

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -9,12 +9,12 @@ const IMAGES = [
 
 
 const ImageList = () => {
-  const RenderedImages = IMAGES.map(function (image) {
-    return <ImageDetail image={image}/>
-  });
+  const RenderedImages = IMAGES.map(image =>
+    <ImageDetail key={image.title} image={image}/>
+  );
 
   return (
-    <ul>
+    <ul className="media-list list-group">
       {RenderedImages}
     </ul>
   );
