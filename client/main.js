a54ba013b46698912736b5b855724fa76081b594
rayGreycloud <rayGreycloud@gmail.com>
2016-08-04 18:28:23 -0700

    Add axios and image api fetch

--- client/main.js
+++ client/main.js
@@ -4,7 +4,19 @@ import axios from 'axios';
 import ImageList from './components/image_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
+  componentWillMount() {
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data}));
+  }
+
   render(){
+    console.log(this.state.images);
     return (
       <div>
         <ImageList />
@@ -15,6 +27,5 @@ class App extends Component {
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then(response => console.log(response));
+
 });
